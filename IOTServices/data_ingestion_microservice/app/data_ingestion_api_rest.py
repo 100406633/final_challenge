f9836097379be010ca096d838749cffc75405673
CosminOctavianPetre <100428943@alumnos.uc3m.es>
2022-05-27 19:49:26 +0200

    web comms WIP

--- IOTServices/data_ingestion_microservice/app/data_ingestion_api_rest.py
+++ IOTServices/data_ingestion_microservice/app/data_ingestion_api_rest.py
@@ -28,30 +28,8 @@ def device_state():
         #     return {"response": "Incorrect parameters"}, 401
         myselect = get_device_state()
         print(myselect, file=sys.stderr)
-        return {1: {"room": myselect[0], "type": myselect[1], "value": myselect[2]}}
+        return [{"room": myselect[0], "type": myselect[1], "value": myselect[2]}]
 
-
-# def device_state():
-#     global json_params,counter
-#     if request.method == 'POST':
-#         params = request.get_json()
-#         if len(params) != 3:
-#             return {"response":"Incorrect parameters"}, 401
-#         mycursor = insert_device_state(params)
-#         json_params[counter]=params
-#         counter+=1
-#         return {"response":f"{mycursor.rowcount} records inserted."},200
-#
-#     if request.method == 'GET':
-#         counter = 0
-#         var = json_params
-#         json_params = {}
-#         return var
-#         """ params = request.get_json()
-#         if len(params) != 3:
-#             return {"response":"Incorrect parameters"}, 401
-#         myselect = select_device_state(params)
-#         return myselect"""
 
 HOST = os.getenv("HOST")
 PORT = os.getenv("PORT")
