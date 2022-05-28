3f6836057bcc425d5d543d4caec09b07c10297f9
CosminOctavianPetre <100428943@alumnos.uc3m.es>
2022-05-28 14:34:19 +0200

    SQL query WIP

--- IOTServices/data_ingestion_microservice/app/data_ingestion.py
+++ IOTServices/data_ingestion_microservice/app/data_ingestion.py
@@ -34,8 +34,8 @@ def insert_device_state(params):
 def get_device_state():
     mydb = connect_database()
     with mydb.cursor() as mycursor:
-        sql = "WITH (SELECT room, type, value, MIN(TIMESTAMPDIFF(SECOND, date, CURRENT_TIMESTAMP)) FROM device_state GROUP BY (room, type, value)) AS A" \
-              "SELECT room, type, value FROM A"
+        sql = "WITH a AS (SELECT room, type, value, MIN(TIMESTAMPDIFF(SECOND, date, CURRENT_TIMESTAMP)) FROM device_state GROUP BY (room, type, value))" \
+              "SELECT room, type, value FROM a"
         # sql = "SELECT room,type,value FROM device_state WHERE room=%s AND type=%s ORDER BY date desc limit 1"
         # sql = "SELECT room,type,value FROM device_state WHERE room='"+ str(params['room']) + "' and type ='" + str(params['type']) + "' ORDER BY date desc limit 1"
         print(f"sql = {sql}", file=sys.stderr)
